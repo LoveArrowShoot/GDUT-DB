c273b830b9a1317291f6923132b181cccc15661a
OuJinJin <1016068816@qq.com>
2023-01-06 00:43:31 +0800

    fix(mapper):对应参数名

--- src/main/java/com/gdutdb/carsales/mapper/InventoryMapper.java
+++ src/main/java/com/gdutdb/carsales/mapper/InventoryMapper.java
@@ -3,6 +3,7 @@ package com.gdutdb.carsales.mapper;
 import com.gdutdb.carsales.po.dto.InventoryDetail;
 import com.gdutdb.carsales.po.poja.Inventory;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,8 +15,8 @@ public interface InventoryMapper extends BaseMapper<Inventory> {
     InventoryDetail queryByCarVin(Integer carVin);
     InventoryDetail queryByDistributorId(Integer distributeId);
     Integer deleteByInventoryId(Integer inventoryId);
-    Integer updateCount(Integer inventoryId, Integer count);
-    Integer changeCount(Integer inventoryId, Integer changeCount);
+    Integer updateCount(@Param("inventoryId") Integer inventoryId,@Param("count") Integer count);
+    Integer changeCount(@Param("inventoryId") Integer inventoryId,@Param("count") Integer count);
 }
 
 
